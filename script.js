f33e6c62af0595f81ea8925c0f867ccf92d1e24a
Daavid-15 <davidsantiagodelafuente@gmail.com>
2025-07-15 15:57:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -97,14 +97,15 @@ captureButton.addEventListener("click", async () => {
     };
 
     // Intentar flash nativo si existe
-    if (caps.fillLightMode?.includes("flash")) {
-      settings.fillLightMode = "flash";
-      debugLog("Usando flash integrado");
-    }
+    //if (caps.fillLightMode?.includes("flash")) {  //Descartado, en mi movil salta el flash descordinado con la imagen
+      //settings.fillLightMode = "flash";
+      //debugLog("Usando flash integrado");
+    //}
     // O activar torch manual
-    else if (caps.torch) {
+    if (caps.torch) {
       await track.applyConstraints({ advanced: [{ torch: true }] });
       debugLog("Linterna activada");
+      await new Promise(r => setTimeout(r, 250)); //Tiempo para estabilizar el led
     }
 
     // Capturar la foto
@@ -114,6 +115,7 @@ captureButton.addEventListener("click", async () => {
 
     // Apagar torch si se encendió
     if (caps.torch) {
+      await new Promise(r => setTimeout(r, 250)); //Tiempo para estabilizar el led
       await track.applyConstraints({ advanced: [{ torch: false }] });
       debugLog("Linterna desactivada");
     }
